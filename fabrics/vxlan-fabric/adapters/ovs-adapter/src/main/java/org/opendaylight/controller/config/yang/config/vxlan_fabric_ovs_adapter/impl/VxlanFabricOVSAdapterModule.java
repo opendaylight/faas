f70a5d269c4d90e07e7a2a5a711cf19d1c2792db
Ziyang Yao <jasonvke@foxmail.com>
2015-10-13 20:53:17 +0800

    Initial check in for faas
    Change-Id: I5aed1d238e67f510a773fd6985c15ceaf03fa9c7
    Signed-off-by: Ziyang Yao <jasonvke@foxmail.com>

---
+++ fabrics/vxlan-fabric/adapters/ovs-adapter/src/main/java/org/opendaylight/controller/config/yang/config/vxlan_fabric_ovs_adapter/impl/VxlanFabricOVSAdapterModule.java
@@ -0,0 +1,28 @@
+package org.opendaylight.controller.config.yang.config.vxlan_fabric_ovs_adapter.impl;
+public class VxlanFabricOVSAdapterModule extends org.opendaylight.controller.config.yang.config.vxlan_fabric_ovs_adapter.impl.AbstractVxlanFabricOVSAdapterModule {
+    public VxlanFabricOVSAdapterModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public VxlanFabricOVSAdapterModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.vxlan_fabric_ovs_adapter.impl.VxlanFabricOVSAdapterModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+    	return new AutoCloseable() {
+
+			@Override
+			public void close() throws Exception {
+				// TODO Auto-generated method stub
+				
+			}
+    	};
+    }
+
+}
---
+++ fabrics/vxlan-fabric/vxlan-fabric/src/main/java/org/opendaylight/controller/config/yang/config/vxlan_fabric/impl/VxlanFabricModule.java
@@ -0,0 +1,28 @@
+package org.opendaylight.controller.config.yang.config.vxlan_fabric.impl;
+public class VxlanFabricModule extends org.opendaylight.controller.config.yang.config.vxlan_fabric.impl.AbstractVxlanFabricModule {
+    public VxlanFabricModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public VxlanFabricModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.vxlan_fabric.impl.VxlanFabricModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+    	return new AutoCloseable() {
+
+			@Override
+			public void close() throws Exception {
+				// TODO Auto-generated method stub
+				
+			}
+    	};
+    }
+
+}
