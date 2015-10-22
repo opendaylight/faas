c861fe39c5cfb631593be2af32a0e46ff12c203f
Danian Shi <shidn@hotmail.com>
2015-10-22 09:58:50 +0800

    change for fabric model 1, change routed rpc to global rpc for clustering support 2, add logic-device parameter in create-logic-port 3, add draft code for fabric management
    Change-Id: I2a6ab5f873ecf7d32e75ece8a774c802e8843b70
    Signed-off-by: Danian Shi <shidn@hotmail.com>

---
+++ fabrics/vxlan-fabric/vxlan-fabric/src/main/java/org/opendaylight/controller/config/yang/config/vxlan_fabric/impl/PredefineFabricModule.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2015 Huawei Technologies Co. Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.config.yang.config.vxlan_fabric.impl;
+public class PredefineFabricModule extends org.opendaylight.controller.config.yang.config.vxlan_fabric.impl.AbstractPredefineFabricModule {
+    public PredefineFabricModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public PredefineFabricModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.vxlan_fabric.impl.PredefineFabricModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
