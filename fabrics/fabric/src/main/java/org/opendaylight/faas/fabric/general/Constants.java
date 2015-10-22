3d61e308fd815ebdef45218c76ded4e81209eb72
Danian Shi <shidn@hotmail.com>
2015-10-22 09:58:50 +0800

    change for fabric model 1, change routed rpc to global rpc for clustering support 2, add logic-device parameter in create-logic-port 3, add draft code for fabric management
    Change-Id: I2a6ab5f873ecf7d32e75ece8a774c802e8843b70
    Signed-off-by: Danian Shi <shidn@hotmail.com>

---
+++ fabrics/fabric/src/main/java/org/opendaylight/faas/fabric/general/Constants.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2015 Huawei Technologies Co. Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.faas.fabric.general;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class Constants {
+
+    public static final String FABRICS_TOPOLOGY_ID = "faas:fabrics";
+
+    public static final InstanceIdentifier<Topology> DOM_FABRICS_PATH = InstanceIdentifier.create(NetworkTopology.class)
+            .child(Topology.class, new TopologyKey(new TopologyId(new Uri(FABRICS_TOPOLOGY_ID))));
+}
