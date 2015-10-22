281c307c3d1c4e20726508808b3c2c657656b767
Danian Shi <shidn@hotmail.com>
2015-10-22 09:58:50 +0800

    changes for fabric model
    1, use global rpc instead of routed rpc
    2, add logic-device parameter in create-logic-port
    3, add draft code for fabric management api (composeFabric, decomposeFabric)
    
    Change-Id: I2a6ab5f873ecf7d32e75ece8a774c802e8843b70
    Signed-off-by: Danian Shi <shidn@hotmail.com>

---
+++ fabrics/fabric/src/main/java/org/opendaylight/faas/fabric/general/FabricFactory.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2015 Huawei Technologies Co. Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.faas.fabric.general;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+
+public class FabricFactory implements AutoCloseable {
+
+    private FabricManagementAPIProvider manageAPI;
+
+    private EndPointRegister epRegister;
+
+    private final ExecutorService executor;
+
+    public FabricFactory (final DataBroker dataProvider,
+            final RpcProviderRegistry rpcRegistry,
+            final NotificationProviderService notificationService) {
+
+        executor = Executors.newFixedThreadPool(1);
+
+        manageAPI = new FabricManagementAPIProvider(dataProvider, rpcRegistry, executor);
+        manageAPI.start();
+
+        epRegister = new EndPointRegister(dataProvider, rpcRegistry, executor);
+        epRegister.start();
+    }
+
+    @Override
+    public void close() throws Exception {
+        manageAPI.close();
+        epRegister.close();
+        executor.shutdown();
+    }
+}
