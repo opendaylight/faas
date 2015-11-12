0fd101fa0ca2636b315cb0f17338c8b5c785bab0
Henry Yu <henry.yu1@huawei.com>
2015-11-12 15:59:48 -0500

    Create FabricMgr Provider class
    * Initial code creation of the fabric manager
    * Modified namespaces in vcontainer yang models
    * Code cleanup in features/pom.xml
    
    Change-Id: I5f8fd6c31c81524ca7e2b498140758c34e42ebec
    Signed-off-by: Henry Yu <henry.yu1@huawei.com>

---
+++ fabric-mgr/fabric-mgr-impl/src/main/java/org/opendaylight/faas/fabricmgr/FabricMgrProvider.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2015 Huawei Technologies Co. Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.faas.fabricmgr;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.binding.api.NotificationService;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class FabricMgrProvider implements AutoCloseable, DataChangeListener {
+
+    public FabricMgrProvider (final DataBroker dataProvider,
+                             final RpcProviderRegistry rpcRegistry,
+                             final NotificationService notificationService) {
+
+    }
+
+    @Override
+    public void onDataChanged(AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> arg0) {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
