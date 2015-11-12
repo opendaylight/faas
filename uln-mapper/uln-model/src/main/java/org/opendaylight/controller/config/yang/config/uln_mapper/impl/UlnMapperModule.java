39f692e4e1cbda78676fa5fc7ee8cfdf652bbf71
Henry Yu <henry.yu1@huawei.com>
2015-11-12 15:59:48 -0500

    Create FabricMgr Provider class
    * Initial code creation of the fabric manager
    * Modified namespaces in vcontainer yang models
    * Code cleanup in features/pom.xml
    
    Change-Id: I5f8fd6c31c81524ca7e2b498140758c34e42ebec
    Signed-off-by: Henry Yu <henry.yu1@huawei.com>

--- uln-mapper/uln-model/src/main/java/org/opendaylight/controller/config/yang/config/uln_mapper/impl/UlnMapperModule.java
+++ uln-mapper/uln-model/src/main/java/org/opendaylight/controller/config/yang/config/uln_mapper/impl/UlnMapperModule.java
@@ -1,6 +1,3 @@
-package org.opendaylight.controller.config.yang.config.uln_mapper.impl;
-
-import org.opendaylight.faas.uln.manager.UlnMapperDatastoreDependency;
 /**
  * Copyright (c) 2015 Huawei Technologies Co. Ltd. and others. All rights reserved.
  *
@@ -8,6 +5,8 @@ import org.opendaylight.faas.uln.manager.UlnMapperDatastoreDependency;
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+package org.opendaylight.controller.config.yang.config.uln_mapper.impl;
+import org.opendaylight.faas.uln.manager.UlnMapperDatastoreDependency;
 import org.opendaylight.faas.uln.manager.UserLogicalNetworkManager;
 
 public class UlnMapperModule extends org.opendaylight.controller.config.yang.config.uln_mapper.impl.AbstractUlnMapperModule {
