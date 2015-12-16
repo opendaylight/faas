6f93ea7176a814807835a6c0ee6f9ffa72f97c1a
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-12-15 23:08:18 -0500

    Add missing license headers
    Change-Id: I278be0e65e49855687d1d4f004a8d7936d131f8b
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- fabrics/vxlan-fabric/vxlan-fabric/src/main/java/org/opendaylight/faas/fabric/vxlan/GatewayPort.java
+++ fabrics/vxlan-fabric/vxlan-fabric/src/main/java/org/opendaylight/faas/fabric/vxlan/GatewayPort.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015 Huawei Technologies Co. Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.opendaylight.faas.fabric.vxlan;
 
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
