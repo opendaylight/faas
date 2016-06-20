28302f07208771f847342293761635796bc02f6f
Danian Shi <shidn@hotmail.com>
2016-06-20 09:30:03 +0800

    add implements for vlan fabric
    fix some issue in yang and feature file.
    
    Change-Id: I45e9d4b79e8e6d012a7639c120bc41ebb116e331
    Signed-off-by: Danian Shi <shidn@hotmail.com>

---
+++ fabrics/vlan-fabric/vlan-fabric/src/main/java/org/opendaylight/faas/fabric/vlan/LogicRouterContext.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2015 Huawei Technologies Co. Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.faas.fabric.vlan;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+public class LogicRouterContext {
+    private final long vrf;
+
+    private Map<Long, GatewayPort> gatewayPorts = Maps.newConcurrentMap();
+
+    private List<String> acls = Lists.newArrayList();
+
+    LogicRouterContext(long vrf) {
+        this.vrf = vrf;
+    }
+
+    public long getVrfCtx() {
+        return vrf;
+    }
+
+    public GatewayPort addGatewayPort(IpPrefix ip, long vni, NodeId lsw) {
+        return gatewayPorts.put(vni, new GatewayPort(ip, vni, lsw, vrf));
+    }
+
+    public GatewayPort removeGatewayPort(long vni) {
+         return gatewayPorts.remove(vni);
+    }
+
+    public GatewayPort getGatewayPortByVni(long vni) {
+        return gatewayPorts.get(vni);
+    }
+
+     public void addAcl(String aclName) {
+          acls.add(aclName);
+     }
+
+     public void removeAcl(String aclName) {
+          acls.remove(aclName);
+     }
+
+     public List<String> getAcls() {
+          return Collections.unmodifiableList(acls);
+     }
+
+     public Set<Long> getVnis() {
+          return gatewayPorts.keySet();
+     }
+}
--- fabrics/vlan-fabric/vlan-fabric/src/main/java/org/opendaylight/faas/fabric/vlan/VlanFabricProvider.java
+++ fabrics/vlan-fabric/vlan-fabric/src/main/java/org/opendaylight/faas/fabric/vlan/VlanFabricProvider.java
@@ -5,49 +5,36 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.faas.fabric.vxlan;
+package org.opendaylight.faas.fabric.vlan;
 
 import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
-import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.faas.fabric.general.FabricRendererRegistry;
 import org.opendaylight.faas.fabric.general.spi.FabricListener;
 import org.opendaylight.faas.fabric.general.spi.FabricRenderer;
 import org.opendaylight.faas.fabric.general.spi.FabricRendererFactory;
-import org.opendaylight.faas.fabric.utils.MdSalUtils;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.capable.device.rev150930.FabricCapableDevice;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.capable.device.rev150930.network.topology.topology.node.Attributes;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.device.adapter.vxlan.rev150930.VtepAttribute;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.device.adapter.vxlan.rev150930.network.topology.topology.node.attributes.Vtep;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.device.adapter.vxlan.rev150930.network.topology.topology.node.attributes.VtepBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.rev150930.ComposeFabricInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.rev150930.FabricId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.rev150930.FabricNode;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.rev150930.fabric.attributes.DeviceNodes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.rev150930.network.topology.topology.node.FabricAttribute;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.rev150930.network.topology.topology.node.FabricAttributeBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.type.rev150930.NodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.type.rev150930.UnderlayerNetworkType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.faas.fabric.vxlan.rev150930.VxlanDeviceComposeInput;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class VXLANFabricProvider implements AutoCloseable, FabricRendererFactory {
+public class VlanFabricProvider implements AutoCloseable, FabricRendererFactory {
 
-    private static final Logger LOG = LoggerFactory.getLogger(VXLANFabricProvider.class);
+    private static final Logger LOG = LoggerFactory.getLogger(VlanFabricProvider.class);
 
     private final DataBroker dataBroker;
     private final RpcProviderRegistry rpcRegistry;
@@ -57,7 +44,7 @@ public class VXLANFabricProvider implements AutoCloseable, FabricRendererFactory
 
     private final Map<InstanceIdentifier<FabricNode>, FabricContext> fabricCtxs = Maps.newHashMap();
 
-    public VXLANFabricProvider(final DataBroker dataProvider,
+    public VlanFabricProvider(final DataBroker dataProvider,
                              final RpcProviderRegistry rpcRegistry,
                              final NotificationPublishService notificationService,
                              final FabricRendererRegistry rendererRegistry) {
@@ -81,27 +68,13 @@ public class VXLANFabricProvider implements AutoCloseable, FabricRendererFactory
 
 
     @Override
-    public FabricRenderer composeFabric(InstanceIdentifier<FabricNode> iid,
-            FabricAttributeBuilder fabric, ComposeFabricInput input) {
-        List<DeviceNodes> devices = input.getDeviceNodes();
-        if (devices != null) {
-            for (DeviceNodes deviceNode : devices) {
-
-                VxlanDeviceComposeInput augment = deviceNode.getAugmentation(VxlanDeviceComposeInput.class);
-                IpAddress vtep = null;
-                if (augment != null) {
-                    vtep = augment.getVtepIp();
-                }
-                if (vtep != null) {
-                    setVtep2Device(deviceNode.getDeviceRef(), vtep);
-                }
-            }
-        }
+    public FabricRenderer composeFabric(InstanceIdentifier<FabricNode> iid, FabricAttributeBuilder fabric,
+            ComposeFabricInput input) {
         FabricId fabricId = new FabricId(iid.firstKeyOf(Node.class).getNodeId());
         FabricContext fabricCtx = new FabricContext(fabricId, dataBroker);
         fabricCtxs.put(iid, fabricCtx);
 
-        return new DistributedFabricRenderer(dataBroker, fabricCtx);
+        return new VlanFabricRenderer(dataBroker, fabricCtx);
     }
 
     @Override
@@ -114,22 +87,6 @@ public class VXLANFabricProvider implements AutoCloseable, FabricRendererFactory
             fabricCtxs.put(iid, fabricCtx);
         }
 
-        return new DistributedFabricListener(iid, dataBroker, rpcRegistry, fabricCtx);
+        return new VlanFabricListener(iid, dataBroker, rpcRegistry, fabricCtx);
     }
-
-    private void setVtep2Device(NodeRef node, IpAddress vtep) {
-        WriteTransaction trans = dataBroker.newWriteOnlyTransaction();
-
-        @SuppressWarnings("unchecked")
-        InstanceIdentifier<Node> nodeIId = (InstanceIdentifier<Node>) node .getValue();
-        InstanceIdentifier<Vtep> vtepIId = nodeIId.augmentation(FabricCapableDevice.class).child(Attributes.class)
-                .augmentation(VtepAttribute.class).child(Vtep.class);
-
-        VtepBuilder builder = new VtepBuilder();
-        builder.setIp(vtep);
-
-        trans.put(LogicalDatastoreType.OPERATIONAL, vtepIId, builder.build(), true);
-        MdSalUtils.wrapperSubmit(trans, executor);
-    }
-
 }
